526dd9cb4cba8c32f3bb23ea376a0918c90d0625
ArtemiyAntnamve <Antnamve@gmail.com>
2023-10-03 01:21:59 +0300

    v. 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,59 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    attr_accessor :old_password
+  attr_accessor :old_password, :remember_token
 
-    has_secure_password validations: false
+  has_secure_password validations: false
 
-    validate :password_presence
-    validate :correct_old_password, on: :update, if: -> { password.present? }
-    validates :password, confirmation: true, allow_blank: true
+  validate :password_presence
+  validate :correct_old_password, on: :update, if: -> { password.present? }
+  validates :password, confirmation: true, allow_blank: true
 
-    validates :email, presence: true, uniqueness: true, 'valid email_2/email': true
-    validate :password_complexity
+  validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
+  validate :password_complexity
 
-    private
+  def remember_me
+    self.remember_token = SecureRandom.urlsafe_base64
+    update_column :remember_token_digest, digest(remember_token)
+  end
 
-    def correct_old_password
-        return if BCrypt::Password.new(password_digest_was).is_password?(old_password)
+  def forget_me
+    update_column :remember_token_digest, nil
+    self.remember_token = nil
+  end
 
-        errors.add :old_password, 'is incorrect'
-    end
-    
-    def password_complexity
-        # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-        return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
-        
-        errors.add :password, 'complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
-    end
+  def remember_token_authenticated?(remember_token)
+    return false if remember_token_digest.blank?
 
-    def password_presence
-        errors.add(:password, :blank) unless password_digest.present?
-    end
+    BCrypt::Password.new(remember_token_digest).is_password?(remember_token)
+  end
+
+  private
+
+  def digest(string)
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost:)
+  end
+
+  def correct_old_password
+    return if BCrypt::Password.new(password_digest_was).is_password?(old_password)
+
+    errors.add :old_password, 'is incorrect'
+  end
+
+  def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+
+    errors.add :password,
+               'complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
+  def password_presence
+    errors.add(:password, :blank) if password_digest.blank?
+  end
 end
